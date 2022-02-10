858f584d54cc870626bbb3d6432be125eff04c0e
kyzsu <l0calh34rtz@gmail.com>
2022-02-10 14:14:16 +0700

    feat: fix tab title

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,7 +9,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Immanuel Williams Irsal - Homepage</title>
+        <title>Home - Immanuel Williams</title>
       </Head>
 
       <Navbar path={router.asPath} />
--- pages/works.js
+++ pages/works.js
@@ -2,27 +2,30 @@ import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import thumbMA from '../public/images/thumbMA.png'
+import Layout from '../components/layouts/article'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        {/* works */}
-        <Section>
-          <WorkGridItem
-            // href="https://metrodataacademy.id/"
-            title="Metrodata Academy"
-            thumbnail={thumbMA}
-            id="metrodata-academy"
-          >
-            A studying web app owned by Metrodata.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+    <Layout title="Works">
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          {/* works */}
+          <Section>
+            <WorkGridItem
+              // href="https://metrodataacademy.id/"
+              title="Metrodata Academy"
+              thumbnail={thumbMA}
+              id="metrodata-academy"
+            >
+              A studying web app owned by Metrodata.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
