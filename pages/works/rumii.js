438916ab3b02b84da84b0a2d10819f403c36ab1a
kyzsu <l0calh34rtz@gmail.com>
2022-06-06 14:26:24 +0700

    feat: delete unused imports

--- pages/works/rumii.js
+++ pages/works/rumii.js
@@ -1,5 +1,4 @@
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Badge, Container, Link, List, ListItem } from '@chakra-ui/react'
+import { Badge, Container, List, ListItem } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
 import { Meta, Title, WorkImage } from '../../components/work'
 
